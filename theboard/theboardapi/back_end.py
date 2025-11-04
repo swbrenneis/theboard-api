ab8cf5c604b6b2c14bbc0d941c0924a12c2f7918
Steve Brenneis <swbrenneis@gmail.com>
2025-11-04 17:20:20 -0500

    Initial commit

---
+++ theboard/theboardapi/back_end.py
@@ -0,0 +1,13 @@
+
+
+class TheBoardBackend:
+    """ Encapsulates the back end API methods for accessing the board enclave """
+
+    def register(self, screen_name):
+        pass
+
+    def login(self):
+        pass
+
+    def logout(self):
+        pass
