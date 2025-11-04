c4eec6ca04423535ab9530883cfba4e31b2df136
Steve Brenneis <swbrenneis@gmail.com>
2025-11-04 17:20:40 -0500

    Continuing coding

--- theboard/theboard/settings.py
+++ theboard/theboard/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'theboard.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'theboard',
+        'USER': 'theboard',
+        'PASSWORD': '8K4BARmEVbwwjyNu',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
