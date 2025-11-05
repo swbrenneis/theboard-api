069ef3c6f4b78d6e389c22332ba7a5d094c767e2
Steve Brenneis <swbrenneis@gmail.com>
2025-11-05 17:03:03 -0500

    Updated model

--- theboard/theboardapi/models.py
+++ theboard/theboardapi/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
 class TheBoardMember(models.Model):
-    screen_name = models.CharField(max_length=20)
-    public_id = models.CharField(max_length=32)
-    enclave_key = models.CharField(max_length=32)
-    signing_key = models.TextField()
-    encryption_key = models.TextField()
+    screen_name = models.CharField(max_length=20, default='')
+    public_id = models.CharField(max_length=32, null=True)
+    enclave_key = models.CharField(max_length=32, null=True)
+    passphrase = models.CharField(max_length=20, default='')
+    public_key = models.CharField(max_length=40, null=True)
+    private_key = models.CharField(max_length=40, null=True)
+    signing_key = models.TextField(max_length=2048, null=True)
+    encryption_key = models.TextField(max_length=4096, null=True)
