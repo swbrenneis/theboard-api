9a12cba21f2ddc4566b68ae3ee5732ef21761bec
stevebrenneis <swbrenneis@foronlyyou.com>
2025-11-06 16:30:15 -0500

    Moved

--- theboard/theboardapi/crypto.py
+++ theboard/theboardapi/crypto.py
@@ -1,8 +1,13 @@
 import string
+import base64
 
-from models import TheBoardMember
+from Crypto.Hash import SHA256
 from Crypto.Random import random
 from Crypto.PublicKey import ECC, RSA
+from Crypto.Signature import DSS
+
+from models import TheBoardMember
+from .exceptions.InvalidSignature import InvalidSignature
 
 
 def generate_passphrase(length=20):
@@ -31,6 +36,7 @@ def generate_member(screen_name):
     member = TheBoardMember()
     member.screen_name = screen_name
 
+    # TODO encrypt password
     passphrase = generate_passphrase()
     member.passphrase = passphrase
 
@@ -48,3 +54,51 @@ def get_ecc_publicKey(pem, passphrase):
     private_key = ECC.import_key(pem_bytes, passphrase=passphrase)
     public_key = private_key.public_key()
     return public_key.export_key(format='PEM')
+
+
+def validate_signature(public_key_pem, fields, signature):
+    """
+    Validates the signature against the given fields
+    :param public_key_pem: PEM encoded ECC signature public key
+    :type public_key_pem: str
+    :param fields: List of fields to hash
+    :type fields: list
+    :param signature: Base64 encoded signature to validate
+    :type signature: str
+    :return None
+    :rtype None
+    :raises InvalidSignature: If the signature is invalid
+    """
+    public_key_bytes = public_key_pem.encode('utf-8')
+    public_key = ECC.import_key(public_key_bytes)
+    signature_bytes = base64.b64decode(signature)
+    h = SHA256.new()
+    for field in fields:
+        h.update(field)
+    verifier = DSS.new(public_key, 'fips-186-3')
+    try:
+        verifier.verify(h, signature_bytes)
+    except ValueError:
+        raise InvalidSignature()
+
+
+def sign_message(private_key_pem, passphrase, fields):
+    """
+    Signs the given fields using the given private_key_pem and passphrase
+    :param private_key_pem: PEM encoded ECC private key
+    :type private_key_pem: str
+    :param passphrase: The passphrase for the private key
+    :type passphrase: str
+    :param fields: List of fields to sign
+    :type fields: list
+    :return The base 64 encoded signature
+    :rtype: str
+    """
+    private_key_bytes = private_key_pem.encode('utf-8')
+    private_key = ECC.import_key(private_key_bytes, passphrase=passphrase)
+    h = SHA256.new()
+    for field in fields:
+        h.update(field)
+    signer = DSS.new(private_key, 'fips-186-3')
+    signed = signer.sign(h)
+    return base64.b64encode(signed)
