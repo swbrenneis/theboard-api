f39692e2739987fd813ff87dbe672c4468351e48
stevebrenneis <swbrenneis@foronlyyou.com>
2025-11-06 16:31:05 -0500

    Changed primary key and modified server public key field

--- theboard/theboardapi/models.py
+++ theboard/theboardapi/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 
 class TheBoardMember(models.Model):
-    screen_name = models.CharField(max_length=20, default='')
+    screen_name = models.CharField(max_length=20, default='', primary_key=True)
     public_id = models.CharField(max_length=32, null=True)
     enclave_key = models.CharField(max_length=32, null=True)
     passphrase = models.CharField(max_length=20, default='')
-    public_key = models.CharField(max_length=40, null=True)
-    private_key = models.CharField(max_length=40, null=True)
+    server_signing_key = models.TextField(max_length=2048, null=True)
     signing_key = models.TextField(max_length=2048, null=True)
     encryption_key = models.TextField(max_length=4096, null=True)
