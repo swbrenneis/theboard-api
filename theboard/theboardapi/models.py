60e5d352e0946ce97ff08cedc80ecb82448aa1fd
Steve Brenneis <swbrenneis@gmail.com>
2025-11-25 17:13:08 -0500

    Added session context object

--- theboard/theboardapi/models.py
+++ theboard/theboardapi/models.py
@@ -8,3 +8,9 @@ class TheBoardMember(models.Model):
     server_signing_key = models.TextField(max_length=2048, null=True)
     signing_key = models.TextField(max_length=2048, null=True)
     encryption_key = models.TextField(max_length=4096, null=True)
+
+class SessionContext(models.Model):
+    screen_name = models.CharField(max_length=100, default='', primary_key=True)
+    session_id = models.CharField(max_length=64, null=True)
+    ephemeral_key = models.CharField(max_length=1024, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
