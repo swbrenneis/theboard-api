fdd2b2570463c87d53bb8eaf15d0cbd2c3b688e4
Steve Brenneis <swbrenneis@foronlyyou.com>
2025-11-25 11:17:19 -0500

    Added ECDH function

--- theboard/theboardapi/crypto2.py
+++ theboard/theboardapi/crypto2.py
@@ -30,3 +30,28 @@ def validate_signature(server_signing_key_pem, fields, signature):
     except Exception as e:
         print(f'Signature verification failed: {e}')
         raise InvalidSignature
+
+def get_ecdh_shared_key(api_private_key, board_public_key):
+    """ Get ECDH shared secret. Shared key is sha256 hash of shared secret """
+    shared = api_private_key.exchange(ec.ECDH(), board_public_key)
+    return hashlib.sha256(shared.encode('utf-8')).digest()
+
+def encrypt_message(cleartext, secret):
+    """ Encrypt cleartext message using AES-GCM. Attach IV to start of message
+    :param cleartext: cleartext message
+    :param secret: secret key
+    :return: encrypted cleartext message base64 encoded
+    """
+    aesgcm = AESGCM(secret)
+    nonce = os.urandom(12)
+    ciphertext = aesgcm.encrypt(cleartext, nonce)
+    encrypted = nonce + ciphertext
+    return base64.b64encode(encrypted).decode('UTF-8')
+
+def decrypt_message(ciphertext, secret):
+    """ Decrypt bae64 encoded text """
+    bytes = base64.b64decode(ciphertext)
+    nonce = bytes[0:12]
+    ciphertext_bytes = bytes[12:]
+    aesgcm = AESGCM(secret)
+    return aesgcm.decrypt(ciphertext_bytes, nonce)
